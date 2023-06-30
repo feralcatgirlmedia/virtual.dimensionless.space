0730e16977748775ae105c87e102b4e3b9c7c22e
FERAL CATGIRL MEDIA <112793706+feralcatgirlmedia@users.noreply.github.com>
2023-06-30 03:32:02 -0400

    Update traktor2m3u.js

--- dj/traktor2m3u.js
+++ dj/traktor2m3u.js
@@ -10,14 +10,23 @@
             var resultContainer = document.getElementById("result-container");
             resultContainer.innerText = m3uData;
             // Create a download link for the converted .m3u file
-            var downloadLink = document.createElement("a");
-            downloadLink.href = "data:text/plain;charset=utf-8," + encodeURIComponent(m3uData);
-            downloadLink.download = traktorInput.name.replace(".nml", ".m3u");
-            downloadLink.innerHTML = '<button>Download Playlist</button>';
-            // Display the download link
-            resultContainer.appendChild(downloadLink);
+            var downloadLinkM3U = createDownloadLink(m3uData, traktorInput.name.replace(".nml", ".m3u"), "Download .m3u");
+            // Create a download link for the track information as a plaintext .txt file
+            var trackInfoText = generateTrackInfoText(traktorData);
+            var downloadLinkText = createDownloadLink(trackInfoText, traktorInput.name.replace(".nml", ".txt"), "Download .txt");
+            // Display the download links
+            resultContainer.appendChild(downloadLinkM3U);
+            resultContainer.appendChild(downloadLinkText);
         };
         reader.readAsText(traktorInput);
+    }
+
+    function createDownloadLink(data, filename, buttonText) {
+        var downloadLink = document.createElement("a");
+        downloadLink.href = "data:text/plain;charset=utf-8," + encodeURIComponent(data);
+        downloadLink.download = filename;
+        downloadLink.innerHTML = '<button>' + buttonText + '</button>';
+        return downloadLink;
     }
 
     function traktorToM3U(traktorData) {
@@ -92,4 +101,32 @@
             }
         }
         return str;
+    }
+
+    function generateTrackInfoText(traktorData) {
+        // Split the Traktor data into individual lines
+        var lines = traktorData.split("\n");
+        // Initialize the track info text
+        var trackInfoText = "";
+        // Process each line of the Traktor data
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            // Check if the line contains a track entry
+            if (line.startsWith("<ENTRY")) {
+                // Extract the track information from the line
+                var title = extractValue(line, "TITLE");
+                var artist = extractValue(line, "ARTIST");
+                var album = extractValue(line, "ALBUM");
+
+                // Replace HTML entities in track information
+                title = replaceHtmlEntities(title);
+                artist = replaceHtmlEntities(artist);
+                album = replaceHtmlEntities(album);
+
+                // Add the track information to the track info text
+                trackInfoText += "Title: " + title + "\n";
+                trackInfoText += "Artist: " + artist + "\n\n";
+            }
+        }
+        return trackInfoText;
     }
